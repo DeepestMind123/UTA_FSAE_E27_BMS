4ffdf1c6c060fae89b343e95c9724313ba3ebfde
verymediocre <Notwerbneb@gmail.com>
2026-06-09 15:10:48 -0500

    DEV_Current Updates
    Moved task timeout from config to inst.
    
    Added flag for different values. Not hyped about name but hey the feature needed to be added.
    
    Added getter function for data diff flag.
    
    Added setter function for task timeout to allow runtime changes to value when BMS changes states.
    
    Added start function for task machine.
    
    Made some minor cosmetic changes.

--- inc/bms/devices/current_sensor/dev_current.c
+++ inc/bms/devices/current_sensor/dev_current.c
@@ -24,6 +24,7 @@ current_status_t DEV_Current_Init(dev_current_t *p_inst, dev_current_cfg_t *p_cf
 
         p_inst->adc_timeout_ms = p_inst->adc_inst->cfg->adc_timeout;
 
+        p_inst->val_diff = false;
         p_inst->is_init = true;
 
         status = CURRENT_STATUS_OK;
@@ -55,9 +56,9 @@ current_status_t DEV_Current_Task(dev_current_t *p_inst)
                 {
                     now_time = UTIL_Time_Get_Tick();
 
-                    if(now_time - p_inst->start_time > p_inst->cfg->current_wait_ms)
+                    if(now_time - p_inst->start_time > p_inst->current_timeout)
                     {
-                        status = ADC_STATUS_ERROR_TIMEOUT;
+                        status = CURRENT_STATUS_ERROR_TIMEOUT;
 
                         p_inst->state = CURRENT_STATE_ERROR;
                     }
@@ -209,6 +210,30 @@ current_status_t DEV_Current_Task(dev_current_t *p_inst)
     return status;
 }
 
+current_status_t DEV_Current_Start(dev_current_t *p_inst)
+{
+    current_status_t status = CURRENT_STATUS_OK;
+
+    if(p_inst != NULL)
+    {
+        if(p_inst->is_init)
+        {
+            p_inst->state = CURRENT_STATE_START;
+        }
+        else 
+        {
+            status = CURRENT_STATUS_ERROR_NOT_INIT;
+        }
+    }
+    else 
+    {
+        status = CURRENT_STATUS_ERROR_NULL_POINTER;
+    }
+
+    return status;
+}
+
+
 current_status_t DEV_Current_Process_Raw(dev_current_t *p_inst)
 {
     current_status_t status = CURRENT_STATUS_OK;
@@ -265,6 +290,11 @@ current_status_t DEV_Current_Process_Raw(dev_current_t *p_inst)
                 current_mA = (delta_mV * MV_TO_UV) / gain_uV;
             }
 
+            if(current_mA != p_inst->last_val)
+            {
+                p_inst->val_diff = true;
+            }
+
             p_inst->last_val = current_mA;
         }
     }
@@ -286,7 +316,7 @@ current_status_t DEV_Current_Get_Wait(dev_current_t *p_inst, uint32_t *p_out)
     {
         if(p_inst->is_init)
         {
-            wait = p_inst->cfg->current_wait_ms;
+            wait = p_inst->current_timeout;
         }
         else
         {
@@ -424,5 +454,59 @@ current_status_t DEV_Current_Get_State(dev_current_t *p_inst, current_state_t *p
 
     return status;
 }
+
+current_status_t DEV_Current_Get_Data_Diff(dev_current_t *p_inst, bool *p_out)
+{
+    current_status_t status = CURRENT_STATUS_OK;
+
+    bool diff_flag = false;
+
+    if((p_inst != NULL) && (p_out != NULL))
+    {
+        if(p_inst->is_init)
+        {
+            diff_flag = p_inst->val_diff;
+        }
+        else 
+        {
+            status = CURRENT_STATUS_ERROR_NOT_INIT;
+        }
+
+        *p_out = diff_flag; 
+    }
+    else 
+    {
+        status = CURRENT_STATUS_ERROR_NULL_POINTER;
+    }
+
+    return status;
+}
+
+current_status_t DEV_Current_Set_Timeout(dev_current_t *p_inst, uint32_t new_val)
+{
+    current_status_t status = CURRENT_STATUS_OK;
+
+    if(p_inst != NULL)
+    {
+        if(p_inst->is_init)
+        {
+            if(new_val) // checks if delay between samples is valid (not 0)
+            {
+                p_inst->current_timeout = new_val;
+            }
+        }
+        else 
+        {
+            status = CURRENT_STATUS_ERROR_NOT_INIT;
+        }
+    }
+    else 
+    {
+        status = CURRENT_STATUS_ERROR_NULL_POINTER;
+    }
+
+    return status;
+}
+
 
 
