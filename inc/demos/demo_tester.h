88b25bc8af5cedca5e725cb6053cced43993cd01
fsae archive, service server <MarcusIsaiahTurley@gmail.com>
2026-06-15 17:25:00 -0500

    | Revision 7 | DEMOS 4 | Marcus Turley | 06-15-26 |
    | CHANGES |
    - Added more demo files
    - Finished 'demo_can.c' demo file
    - Updated 'demo_adc.c'
    
    | ACTIVE |
    - Make more demos

--- inc/demos/demo_tester.h
+++ inc/demos/demo_tester.h
@@ -10,8 +10,23 @@
 
 #include <stdint.h>
 
+static char watch[100]; //watch the 'watch' variable to debug
+
 uint32_t demo_tester();
+
 uint32_t demo_adc_fault();
 uint32_t demo_adc_get(uint32_t p_cycles);
+
+
+uint32_t demo_can_fault();
+
+
+uint32_t demo_i2c_fault();
+
+
+uint32_t demo_pwm_fault();
+
+
+uint32_t demo_spi_fault();
 
 #endif
