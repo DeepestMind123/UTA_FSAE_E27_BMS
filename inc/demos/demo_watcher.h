7ee5fc5ce7ff8640e89f25628b2b02a5a66ab23e
fsae archive, service server <MarcusIsaiahTurley@gmail.com>
2026-06-17 23:36:44 -0500

    | Revision 9 | DEMOS 7 & QOL | Marcus Turley | 06-17-26 |
    | CHANGES |
    - Updated wathc files for ADC and CAN
    - Added watch files for PWM, I2C, and SPI
    
    | ACTIVE |
    - Fix 'get_value' variable not showing up in watcher
    - Integrate bms devices into demo watch files

--- inc/demos/demo_watcher.h
+++ inc/demos/demo_watcher.h
@@ -10,9 +10,9 @@
 
 #define DEMO_ADC
 #define DEMO_CAN
-//#define DEMO_I2C
-//#define DEMO_PWM
-//#define DEMO_SPI
+#define DEMO_I2C
+#define DEMO_PWM
+#define DEMO_SPI
 
 #include <stdint.h>
 #include <string.h>
@@ -20,9 +20,13 @@
 #define ISMAXINT(x) (x == UINT64_MAX || x == UINT32_MAX || x == UINT16_MAX || x == UINT8_MAX)
 
 extern uint64_t get_val;
+extern uint64_t get_val_2;
 extern uint64_t set_val;
+extern uint64_t set_val_2;
+extern uint8_t test_id;
+char driver_name[15];
 
-// This structure defines a demo peripheral
+// This structure defines a driver demo
 typedef struct {
     const char **func_strings;   // Array of function names
     const char **state_strings;  // Array of state names
@@ -30,35 +34,43 @@ typedef struct {
     const int get_state_func_id;
     
     // Generic wrapper for printing demo information
-    int (*execute_func)(int func_id, bool non_null_inst, bool non_null_conf);
-} periph_watcher_t;
+    int (*execute_func)(int p_func_id, bool p_non_null_inst, bool p_non_null_conf);
+} driver_watcher_interface_t;
 
 // Purely generic testing engine functions
-void init_driver(periph_watcher_t *driver);
+void init_driver_watcher(char *p_driver_name, uint8_t p_test_id, driver_watcher_interface_t *p_driver);
 void watch_inst_conf_set(int p_func_id, int p_status_exp, int p_set_val, bool p_not_inst_null, bool p_not_conf_null);
+void watch_inst_conf_set_2(int p_func_id, int p_status_exp, int p_set_val, int p_set_val_2, bool p_not_inst_null, bool p_not_conf_null);
 void watch_inst_conf(int p_func_id, int p_status_exp, bool p_not_inst_null, bool p_not_conf_null);
 
-uint32_t demo_watcher();
+void demo_watcher();
 
 #ifdef DEMO_ADC
-uint32_t demo_watch_adc_fault();
-uint32_t demo_watch_adc_get(uint32_t p_cycles);
+
+void demo_watch_adc_all();
+void demo_watch_adc_funcs();
+void demo_watch_adc_null();
+void demo_watch_adc_task();
 #endif
 
 #ifdef DEMO_CAN
-uint32_t demo_watch_can_fault();
+void demo_watch_can_all();
+void demo_watch_can_funcs();
 #endif
 
 #ifdef DEMO_I2C
-uint32_t demo_watch_i2c_fault();
+void demo_watch_i2c_all();
+void demo_watch_i2c_funcs();
 #endif
 
 #ifdef DEMO_PWM
-uint32_t demo_watch_pwm_fault();
+void demo_watch_pwm_all();
+void demo_watch_pwm_funcs();
 #endif
 
 #ifdef DEMO_SPI
-uint32_t demo_watch_spi_fault();
+void demo_watch_spi_all();
+void demo_watch_spi_funcs();
 #endif
 
 #endif
--- inc/demos/io/demo_watch_adc.c
+++ inc/demos/io/demo_watch_adc.c
@@ -13,8 +13,11 @@
 
 #ifdef DEMO_ADC
 // Initialization
-static int dummy(void) { return 0; }    // Dummy empty function
-static int dummy2(int _) { return 0; }  // Dummy with a number parameter since it needs a place for that value
+static void ADC_channel_select(uint8_t channel) {};
+static void ADC_start(void)                     {};
+static uint8_t ADC_done(void)                   {};
+static uint32_t ADC_get_result(void)            {};
+static void ADC_stop(void)                      {};
 static io_adc_cfg_t config = {
     .adc_timeout            = 1000,
     .adc_resolution         = 8,
@@ -22,15 +25,13 @@ static io_adc_cfg_t config = {
     .adc_vref_mV            = 3300,
     .channel_id             = 0,
 
-    .ADC_channel_select     = (void (*)(uint8_t))dummy2,
-    .ADC_start              = (void (*)(void))dummy,
-    .ADC_done               = (uint8_t (*)(void))dummy,
-    .ADC_get_result         = (uint32_t (*)(void))dummy,
-    .ADC_stop               = (void (*)(void))dummy
+    .ADC_channel_select     = ADC_channel_select,
+    .ADC_start              = ADC_start,
+    .ADC_done               = ADC_done,
+    .ADC_get_result         = ADC_get_result,
+    .ADC_stop               = ADC_stop
 };
 static io_adc_t inst;
-static const io_adc_cfg_t *config_null  = NULL;
-static const io_adc_t *inst_null        = NULL;
 
 // Watch strings
 static const char *func_str[] = {
@@ -68,11 +69,26 @@ static const char *status_str[] = {
     //!NOTE! Add status here
     //"NEW STATUS"
 };
+// Helpful For ID Matching
+typedef enum {
+    ADC_INIT = 0,
+    ADC_TASK,
+    ADC_START,
+    ADC_GET_VAL,
+    ADC_GET_RES,
+    ADC_SET_OFFSET,
+    ADC_GET_OFFSET,
+    ADC_GET_VREF,
+    ADC_GET_STATE,
+    ADC_GET_TIMEOUT,
+    //!NOTE! Add func id here
+    //"NEW FUNC ID"
+} func_id;
 // Get func output to compare non-null vs. null
-int get_func(int p_func_id, bool p_not_inst_null, bool p_not_conf_null) {
-    get_val         = UINT64_MAX;
-    io_adc_t *_inst    = p_not_inst_null ? &inst : NULL;
-    io_adc_cfg_t *_config = p_not_conf_null ? &config : NULL;
+static int get_func(int p_func_id, bool p_not_inst_null, bool p_not_conf_null) {
+    get_val                 = UINT64_MAX;
+    io_adc_t *_inst         = p_not_inst_null ? &inst : NULL;
+    io_adc_cfg_t *_config   = p_not_conf_null ? &config : NULL;
     
     switch(p_func_id) {
         case 0: return (uint64_t)IO_ADC_Init(_inst, _config);              break;
@@ -90,84 +106,107 @@ int get_func(int p_func_id, bool p_not_inst_null, bool p_not_conf_null) {
     }
     set_val = UINT64_MAX;
 }
-static periph_watcher_t adc_test_interface = {
-    .func_strings   = func_str,
-    .state_strings  = states_str,
-    .status_strings = status_str,
-    .execute_func   = get_func,
-    .get_state_func_id = 8
+static driver_watcher_interface_t drive_watcher = {
+    .func_strings       = func_str,
+    .state_strings      = states_str,   //!NOTE! Set to NULL if there are no states
+    .status_strings     = status_str,
+    .execute_func       = get_func,
+    .get_state_func_id  = 8          //!NOTE! Set value to get state func id
 };
 
-// General ADC Test Method
-uint32_t demo_watch_adc_fault() {
-    init_driver(&adc_test_interface);
+// Tests all adc methods
+void demo_watch_adc_all() {
+    demo_watch_adc_funcs();
+//    demo_watch_adc_null();
+//    demo_watch_adc_task();
+}
+
+// General ADC test method that goes through every function
+void demo_watch_adc_funcs() {
+    init_driver_watcher("ADC", 1, &drive_watcher); //!BREAK! Use this as breakpoint
     
     // Action Methods
-    int _func_id = 0, _status_exp = 0; //!BREAK! Use this as breakpoint
-    _func_id = 0;   // ADC INIT
-    watch_inst_conf(_func_id, _status_exp, true, true);
-    watch_inst_conf(_func_id, _status_exp, true, false);
-    watch_inst_conf(_func_id, _status_exp, false, true);
-    watch_inst_conf(_func_id, _status_exp, false, false);
+    // [0] ADC INIT
+    watch_inst_conf(ADC_INIT, ADC_STATUS_OK, true, true);
+    watch_inst_conf(ADC_INIT, ADC_STATUS_ERROR_NULL_POINTER, true, false);
+    watch_inst_conf(ADC_INIT, ADC_STATUS_ERROR_NULL_POINTER, false, true);
+    watch_inst_conf(ADC_INIT, ADC_STATUS_ERROR_NULL_POINTER, false, false);
     
-    _func_id++;     // ADC TASK
-    watch_inst_conf(_func_id, _status_exp, true, true);
-    watch_inst_conf(_func_id, _status_exp, false, false);
+    // [1] ADC TASK
+    watch_inst_conf(ADC_TASK, ADC_STATUS_OK, true, true);
+    watch_inst_conf(ADC_TASK, ADC_STATUS_ERROR_NULL_POINTER, false, false);
     
-    _func_id++;     // ADC START
-    watch_inst_conf(_func_id, _status_exp, true, true);
-    watch_inst_conf(_func_id, _status_exp, false, false);
+    // [2] ADC START
+    watch_inst_conf(ADC_START, ADC_STATUS_OK, true, true);
+    watch_inst_conf(ADC_START, ADC_STATUS_ERROR_NULL_POINTER, false, false);
     
-    _func_id++;     // ADC GET VAL
-    watch_inst_conf(_func_id, _status_exp, true, true);     
-    watch_inst_conf(_func_id, _status_exp, false, false);
+    // [3] ADC GET VAL
+    watch_inst_conf(ADC_GET_VAL, ADC_STATUS_OK, true, true);     
+    watch_inst_conf(ADC_GET_VAL, ADC_STATUS_ERROR_NULL_POINTER, false, false);
     
-    _func_id++;     // ADC GET RES
-    watch_inst_conf(_func_id, _status_exp, true, true);
-    watch_inst_conf(_func_id, _status_exp, false, false);
+    // [4] ADC GET RES
+    watch_inst_conf(ADC_GET_RES, ADC_STATUS_OK, true, true);
+    watch_inst_conf(ADC_GET_RES, ADC_STATUS_ERROR_NULL_POINTER, false, false);
     
-    _func_id++;     // ADC SET OFFSET
-    watch_inst_conf(_func_id, _status_exp, true, true);
-    watch_inst_conf(_func_id, _status_exp, false, false);
+    // [5] ADC SET OFFSET
+    watch_inst_conf_set(ADC_SET_OFFSET, ADC_STATUS_OK, 10, true, true);
+    watch_inst_conf_set(ADC_SET_OFFSET, ADC_STATUS_ERROR_NULL_POINTER, 20, false, false);
     
-    _func_id++;     // ADC GET OFFSET
-    watch_inst_conf_set(_func_id, _status_exp, 10, true, true);
-    watch_inst_conf_set(_func_id, _status_exp, 20, false, false);
+    // [6] ADC GET OFFSET
+    watch_inst_conf(ADC_GET_OFFSET, ADC_STATUS_OK, true, true);
+    watch_inst_conf(ADC_GET_OFFSET, ADC_STATUS_ERROR_NULL_POINTER, false, false);
     
-    _func_id++;     // ADC GET VREF
-    watch_inst_conf(_func_id, _status_exp, true, true);
-    watch_inst_conf(_func_id, _status_exp, false, false);
+    // [7] ADC GET VREF
+    watch_inst_conf(ADC_GET_VREF, ADC_STATUS_OK, true, true);
+    watch_inst_conf(ADC_GET_VREF, ADC_STATUS_ERROR_NULL_POINTER, false, false);
     
-    _func_id++;     // ADC GET STATE
-    watch_inst_conf(_func_id, _status_exp, true, true);
-    watch_inst_conf(_func_id, _status_exp, false, false);
+    // [8] ADC GET STATE
+    watch_inst_conf(ADC_GET_STATE, ADC_STATUS_OK, true, true);
+    watch_inst_conf(ADC_GET_STATE, ADC_STATUS_ERROR_NULL_POINTER, false, false);
     
-    _func_id++;     // ADC GET TIMEOUT
-    watch_inst_conf(_func_id, _status_exp, true, true);
-    watch_inst_conf(_func_id, _status_exp, false, false);
+    // [9] ADC GET TIMEOUT
+    watch_inst_conf(ADC_GET_TIMEOUT, ADC_STATUS_OK, true, true);
+    watch_inst_conf(ADC_GET_TIMEOUT, ADC_STATUS_ERROR_NULL_POINTER, false, false);
     
     //!NOTE! Add new function to track here
-    //_test_id++;     // ADC NEW FUNC
-    //track_inst_conf(_test_id, _status_exp, false, false);
+    //// [#] ADC NEW FUNC
+    //track_inst_conf(ADC_FUNC_ID, ADC_STATUS_ID, false, false);
     
     asm("NOP"); //!BREAK! Use this as breakpoint
-    return 0;
+}
+
+// Demo to test when no init called
+void demo_watch_adc_null() {
+    init_driver_watcher("ADC", 2, &drive_watcher); //!BREAK! Use this as breakpoint
+    
+    // [1] ADC TASK
+    watch_inst_conf(ADC_TASK, ADC_STATUS_ERROR_NULL_POINTER, true, true);    // State: NULL_POINTER
+    
+    // [0] ADC INIT
+    watch_inst_conf(ADC_INIT, ADC_STATUS_ERROR_NULL_POINTER, true, true);    //
+    
+    // [1] ADC TASK
+    watch_inst_conf(ADC_TASK, ADC_STATUS_ERROR_NULL_POINTER, false, false);  // State: NULL_POINTER
 }
  
-// !LATER! Currently unimplemented get value method
-uint32_t demo_watch_adc_get(uint32_t p_cycles) {
-//        IO_ADC_Task(&_adc_1);
-//        snprintf(log, sizeof(log), "|Cycle: %lu|ADC State:%s|Data Read: %lu|", (unsigned long)i, state, (unsigned long)_val);
-//        IO_ADC_Start(&_adc_1);
-//    
-//    IO_ADC_Init(&_adc1, &adc_config);
-//    for(uint32_t i = 1; i <= p_cycles+1; i++) {
-//        IO_ADC_Task(&_adc1);
-//        uint32_t _val;
-//        char *state = adc_states[IO_ADC_Get_Val(&_adc1, &_val)];
-//        char log[100];
-//        snprintf(log, sizeof(log), "|Cycle: %lu|ADC State:%s|Data Read: %lu|", (unsigned long)i, state, (unsigned long)_val);
-//        IO_ADC_Start(&_adc1);
-//    }
+// Demo to test different task states
+void demo_watch_adc_task() {
+    init_driver_watcher("ADC", 3, &drive_watcher); //!BREAK! Use this as breakpoint
+    
+    // [0] ADC INIT
+    watch_inst_conf(ADC_INIT, ADC_STATUS_OK, true, true);
+    
+    // [1] ADC TASK
+    watch_inst_conf(ADC_TASK, ADC_STATUS_OK, true, true);
+    
+    // [2] ADC START
+    watch_inst_conf(ADC_START, ADC_STATUS_OK, true, true);
+    
+    // [1] ADC TASK
+    watch_inst_conf(ADC_TASK, ADC_STATUS_OK, true, true);  // State: START>>>WAIT
+    watch_inst_conf(ADC_TASK, ADC_STATUS_OK, true, true);  // State: WAIT>>>GET
+    watch_inst_conf(ADC_TASK, ADC_STATUS_OK, true, true);  // State: GET>>>READY
+    watch_inst_conf(ADC_TASK, ADC_STATUS_OK, true, true);  // State: READY>>>IDLE
+    watch_inst_conf(ADC_TASK, ADC_STATUS_OK, true, true);  // State: IDLE
 }
 #endif
