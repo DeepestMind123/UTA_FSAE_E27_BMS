1c5f1fdd79360e79b11b8f232ab17021edf3459f
verymediocre <Notwerbneb@gmail.com>
2026-06-18 10:39:32 -0500

    Rename to fan control
    maybe this one will stick

--- cmake/LV_BMS_V1.0/default/.generated/file.cmake
+++ cmake/LV_BMS_V1.0/default/.generated/file.cmake
@@ -17,7 +17,6 @@ endforeach()
 
 set(LV_BMS_V1_0_default_toolchain_XC8_3_10_FILE_TYPE_compile
     "${CMAKE_CURRENT_SOURCE_DIR}/../../../inc/bms/devices/current_sensor/dev_current.c"
-    "${CMAKE_CURRENT_SOURCE_DIR}/../../../inc/bms/devices/fan_control/dev_fan.c"
     "${CMAKE_CURRENT_SOURCE_DIR}/../../../inc/bms/devices/ltc6813/ADI.c"
     "${CMAKE_CURRENT_SOURCE_DIR}/../../../inc/bms/devices/ltc6813/LTC6813.c"
     "${CMAKE_CURRENT_SOURCE_DIR}/../../../inc/bms/devices/ltc6813/dev_ltc6813.c"
@@ -28,7 +27,26 @@ set(LV_BMS_V1_0_default_toolchain_XC8_3_10_FILE_TYPE_compile
     "${CMAKE_CURRENT_SOURCE_DIR}/../../../inc/bms/io/spi_io/io_spi.c"
     "${CMAKE_CURRENT_SOURCE_DIR}/../../../inc/bms/system/bms_fault.c"
     "${CMAKE_CURRENT_SOURCE_DIR}/../../../inc/bms/system/bms_manager.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../inc/bms/utils/ekf_library/rtGetNan.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../inc/bms/utils/ekf_library/rt_nonfinite.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../inc/bms/utils/ekf_library/util_single_cell_ekf.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../inc/bms/utils/util_fan_ctrl/util_fan_ctrl.c"
     "${CMAKE_CURRENT_SOURCE_DIR}/../../../inc/bms/utils/util_time.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../inc/demos/demo_tester.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../inc/demos/demo_watcher.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../inc/demos/devices/demo_current_sensor.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../inc/demos/devices/demo_fan_control.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../inc/demos/devices/demo_ltc6813.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../inc/demos/io/demo_adc.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../inc/demos/io/demo_can.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../inc/demos/io/demo_i2c.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../inc/demos/io/demo_pwm.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../inc/demos/io/demo_spi.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../inc/demos/io/demo_watch_adc.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../inc/demos/io/demo_watch_can.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../inc/demos/io/demo_watch_i2c.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../inc/demos/io/demo_watch_pwm.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../inc/demos/io/demo_watch_spi.c"
     "${CMAKE_CURRENT_SOURCE_DIR}/../../../main.c")
 set_source_files_properties(${LV_BMS_V1_0_default_toolchain_XC8_3_10_FILE_TYPE_compile} PROPERTIES LANGUAGE C)
 set(LV_BMS_V1_0_default_toolchain_XC8_3_10_FILE_TYPE_link)
