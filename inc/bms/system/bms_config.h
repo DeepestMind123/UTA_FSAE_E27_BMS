667cefa2e5db91fa42d7e08152d0a5f898f78e9f
verymediocre <Notwerbneb@gmail.com>
2026-07-13 15:55:18 -0500

    update: bms_config
    switching from structs to defines
    its ugly but better for speed and consistency

--- inc/bms/system/bms_config.h
+++ inc/bms/system/bms_config.h
@@ -31,4 +31,19 @@ typedef struct
 
 } bms_config_t;
 
+#define MOD_CELL_NUM 28
+#define MOD_TEMP_NUM 16
+#define MOD_BAL_IC_NUM 0
+#define MOD_BMS_IC_NUM 2
+#define MOD_NUM 5
+
+#define TOTAL_BMS_IC_NUM (MOD_BMS_IC_NUM * MOD_NUM)
+#define TOTAL_CELL_NUM (MOD_CELL_NUM * MOD_NUM)
+#define CELLS_PER_BMS_IC (MOD_CELL_NUM / MOD_BMS_IC_NUM)
+
+#define UV_LIM_MV 2500
+#define OV_LIM_MV 4200
+#define OT_LIM_DC 600
+#define OC_LIM_MA 600000
+
 #endif
