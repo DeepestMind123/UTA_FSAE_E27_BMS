d7df0d38960611cd810dbbcfd0be31ec34dcb569
verymediocre <Notwerbneb@gmail.com>
2026-07-14 17:34:09 -0500

    update: bms_config
    remove struct

--- inc/bms/system/bms_config.h
+++ inc/bms/system/bms_config.h
@@ -8,29 +8,6 @@
 #ifndef BMS_CONFIG_H
 #define BMS_CONFIG_H
 
-#include <stdint.h>
-#include <stdbool.h>
-
-typedef struct
-{
-    // physical configuration
-    uint8_t module_cell_count;         // number of cells
-    uint8_t module_temp_count;         // number of pack thermistors
-    uint8_t balance_ic_count;   // number of active balancing ics (typically number of cells - 1)
-    uint8_t module_count;
-
-    // limits
-    uint16_t uv_lim_mV;         // under-voltage limit
-    uint16_t ov_lim_mV;         // over-voltage limit
-    uint16_t ot_lim_C;          // over-temperature limit
-    uint32_t oc_lim_mA;         // over-charge current limit
-
-    // capacity
-    uint32_t capacity_mAh;      // nominal capacity in mAh
-    uint16_t idle_current_mA;   // idle current of any onboard electronics (bms)
-
-} bms_config_t;
-
 #define MOD_CELL_NUM 28
 #define MOD_TEMP_NUM 16
 #define MOD_BAL_IC_NUM 0
