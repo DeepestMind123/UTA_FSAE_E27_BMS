151b88b3ab771c946c97db0ec35a7b20b15c8b8e
verymediocre <Notwerbneb@gmail.com>
2026-07-14 17:34:36 -0500

    update: bms_task_manager
    started refactor/rebuild

--- inc/bms/system/bms_task_manager.h
+++ inc/bms/system/bms_task_manager.h
@@ -15,62 +15,39 @@
 
 #include "util_time.h"
 #include "bms_config.h"
-
-#include "dev_current_sensor.h"
-
-#define SENSOR_HANDOFF_MIN 7000L
-#define SENSOR_HANDOFF_MAX 7500L
-#define MAX_CELLS_PER_MODULE 255
-#define MAX_TEMPS_PER_MODULE 255
+#include "bms_daq.h"
 
 
 typedef enum
 {
-   BMS_STATUS_OK = 0,
-   BMS_STATUS_ERROR_NOT_INIT,
-   BMS_STATUS_ERROR_FAULT,
-   BMS_STATUS_ERROR_UNDEFINED_STATE,
-   BMS_STATUS_ERROR_NULL_POINTER,
-   BMS_STATUS_ERROR_CURRENT_SENSOR_FAILURE,
-   BMS_STATUS_MAX
-} bms_status_t;
+   TM_OK = 0,
+   TM_NOT_INIT,
+   TM_DBL_INIT,
+   TM_UNDEF_STATE,
+   TM_NULL_PTR,
+   TM_TIME_FAULT,
+   TM_DAQ_FAULT,
+   TM_STATUS_MAX
+} tm_status_t;
 
 typedef enum
 {
-   BMS_STATE_UNDEFINED = 0,
-   BMS_STATE_IDLE,    // battery inactive, mcu and peripheral components enter sleep mode
-   BMS_STATE_DISCHARGE,   // battery discharging; bms actively checking current draw, cell voltage, thermistors, and updating soc
-   BMS_STATE_CHARGE,      // battery charging; bms balancing cells and confirming CC-CV scheme
-   BMS_STATE_BALANCE,     // battery balancing; bms switching between targeted and general balancing
-   BMS_STATE_FAULT,         // battery fault occured; bms attempts to diagnosis and fix or fails the pack
-   BMS_STATE_MAX
-} bms_state_t;
-
-typedef enum
-{
-   BMS_DISCHARGE_STATE_UNDEFINED = 0,
-   BMS_DISCHARGE_STATE_IDLE,
-   BMS_DISCHARGE_STATE_REPORT,
-   BMS_DISCHARGE_STATE_ERROR,
-   BMS_DISCHARGE_STATE_MAX
-} bms_discharge_state_t;
+   TM_STATE_IDLE = 0,    // battery inactive, mcu and peripheral components enter sleep mode
+   TM_STATE_DISCHARGE,   // battery discharging; bms actively checking current draw, cell voltage, thermistors, and updating soc
+   TM_STATE_CHARGE,      // battery charging; bms balancing cells and confirming CC-CV scheme
+   TM_STATE_BALANCE,     // battery balancing; bms switching between targeted and general balancing
+   TM_STATE_ERROR,         // battery fault occured; bms attempts to diagnosis and fix or fails the pack
+   TM_STATE_MAX
+} tm_state_t;
 
 typedef struct
 {
-   uint16_t module_cells_mV[MAX_CELLS_PER_MODULE];
-   uint16_t module_temps_dC[MAX_TEMPS_PER_MODULE];
-} bms_module_state_t;
-
-typedef struct
-{
-   int16_t last_current_mA;
-   uint32_t last_time_ms;
-} bms_pack_state_t;
+   daq_timeout_t timeout_cfg;
+   util_time_t *time_cfg;
+} tm_cfg_t;
 
 
-bms_status_t BMS_Manager_Init(void);            // intialize system manager with relevant constants and functions, also initializes sub-modules
-bms_status_t BMS_Manager_Task(void);            // overall system state machine
-bms_status_t BMS_Manager_Discharge_Task(void);  // state machine for when bms detects discharging
-bms_status_t BMS_Manager_Current_Sensor_Task(void); // decides which current sensor to use and limits hysteresis 
+tm_status_t BMS_Manager_Init(const tm_cfg_t *p_cfg);            // intialize system manager with relevant constants and functions, also initializes sub-modules
+tm_status_t BMS_Manager_Task(void);            // overall system state machine
 
 #endif
