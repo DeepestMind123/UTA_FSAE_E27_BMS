93808809e5602fad6dd6edf0ebc8cfa0e06be641
verymediocre <Notwerbneb@gmail.com>
2026-08-12 13:45:24 -0500

    update: dev_tsense
    fixed some initialization and safety issues
    
    added status latching to task()
    
    still needs some support for config safety in process func

--- inc/bms/devices/temp_sensor/dev_tsense.h
+++ inc/bms/devices/temp_sensor/dev_tsense.h
@@ -30,8 +30,9 @@ typedef enum
     TSENSE_TIME_FAULT,
     TSENSE_ADC_FAULT,
     TSENSE_UNDEF_STATE,
-    TSENSE_MISMATCH_STATE,
+    TSENSE_INVALID_CFG,
     TSENSE_CONVERSION_FAIL,
+    TSENSE_UNKNOWN_ERROR,
     TSENSE_STATUS_MAX
 } tsense_status_t;
 
@@ -74,46 +75,42 @@ typedef struct
 {
     bool(*temp_start)(tsense_system_t *p_sys);
     bool(*temp_state)(tsense_system_t *p_sys);
-    bool(*temp_stop)(tsense_system_t *p_sys);
     bool(*temp_get_result)(tsense_system_t *p_sys);
 } tsense_func_t;
 
 typedef struct 
 {
-    adc_t *adc_cfg;
-    util_time_t *time_cfg;
-    tsense_func_t *func_cfg;
-    tsense_ctx_t *init_ctx;
+    const adc_t *adc_cfg;
+    const util_time_t *time_cfg;
+    const tsense_func_t *func_cfg;
+    const tsense_ctx_t *init_ctx;
 }tsense_cfg_t;
 
 typedef struct
 {
     uint32_t start_time;
-    uint32_t tsense_timeout_ms;
 
     bool is_init;
     bool is_ready;
-    bool val_diff;
-    
-    tsense_system_t sensor;
-    tsense_func_t *p_func;
+
     tsense_state_t state;
-    adc_t *p_adc;
-    util_time_t *p_time;
-    tsense_ctx_t *p_ctx;
+    tsense_status_t status;
+    
+    const tsense_ctx_t *p_ctx;
+    tsense_system_t sensor;
+    const tsense_func_t *p_func;
+    const adc_t *p_adc;
+    const util_time_t *p_time;
 } tsense_t;
 
 tsense_status_t DEV_Tsense_Init(tsense_t *p_inst, 
-                                const tsense_cfg_t *p_cfg,
-                                const tsense_func_t *p_func,
-                                const adc_t *p_adc,
-                                const util_time_t *p_time);
+                                const tsense_cfg_t *p_cfg);
 
 tsense_status_t DEV_Tsense_Task(tsense_t *p_inst);
 
 tsense_status_t DEV_Tsense_Start(tsense_t *p_inst);
 
-tsense_status_t DEV_Tsense_Process_Raw(tsense_t *p_inst, uint16_t p_in, float *p_out);
+tsense_status_t DEV_Tsense_Process_Raw(tsense_t *p_inst, float p_in, float *p_out);
 
 tsense_status_t DEV_Tsense_Get_Val(tsense_t *p_inst, tsense_val_t (*p_out)[SMALL_ARR_16]);
 
