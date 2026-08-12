7cb8810052730fadce774ebf74f25233d1744d17
verymediocre <Notwerbneb@gmail.com>
2026-08-12 16:26:32 -0500

    update: bms_eval
    added an evaluation for power limits
    
    fixed some minor issues

--- inc/bms/system/eval/bms_eval.c
+++ inc/bms/system/eval/bms_eval.c
@@ -12,15 +12,18 @@ typedef struct
     bool icon_done;
     bool vcon_done;
     bool tcon_done;
+    bool plim_done;
 } eval_ctx_t;
 
 
 typedef struct
 {
     bool is_init;
+
     eval_ctx_t ctx;
     eval_con_t con;
     daq_data_t data;
+
     eval_state_t state;
     eval_status_t status;
 } eval_t;
@@ -38,8 +41,10 @@ eval_status_t BMS_Eval_Init(void)
         s_eval.ctx.icon_done = false;
         s_eval.ctx.vcon_done = false;
         s_eval.ctx.tcon_done = false;
+        s_eval.ctx.plim_done = false;
 
         s_eval.is_init = true;
+        status = EVAL_OK;
     }
     else
     {
@@ -82,6 +87,10 @@ eval_status_t BMS_Eval_Task(const daq_data_t *p_data_in, eval_con_t *p_out)
                     {
                         s_eval.state = EVAL_STATE_TCON;
                     }
+                    else if(!s_eval.ctx.plim_done)
+                    {
+                        s_eval.state = EVAL_STATE_PLIM;
+                    }
                     else
                     {
                         s_eval.state = EVAL_STATE_DONE;
@@ -92,7 +101,7 @@ eval_status_t BMS_Eval_Task(const daq_data_t *p_data_in, eval_con_t *p_out)
 
                 case EVAL_STATE_ICON:
                 {
-                    s_eval.status = BMS_Eval_Icon(&data, &p_out);
+                    s_eval.status = BMS_Eval_Icon(&data, p_out);
 
                     if(s_eval.status == EVAL_OK)
                     {
@@ -110,7 +119,7 @@ eval_status_t BMS_Eval_Task(const daq_data_t *p_data_in, eval_con_t *p_out)
 
                 case EVAL_STATE_VCON:
                 {
-                    s_eval.status = BMS_Eval_Vcon(&data, &p_out);
+                    s_eval.status = BMS_Eval_Vcon(&data, p_out);
 
                     if(s_eval.status == EVAL_OK)
                     {
@@ -128,11 +137,29 @@ eval_status_t BMS_Eval_Task(const daq_data_t *p_data_in, eval_con_t *p_out)
 
                 case EVAL_STATE_TCON:
                 {
-                    s_eval.status = BMS_Eval_Tcon(&data, &p_out);
+                    s_eval.status = BMS_Eval_Tcon(&data, p_out);
 
                     if(s_eval.status == EVAL_OK)
                     {
                         s_eval.ctx.tcon_done = true;
+
+                        s_eval.state = EVAL_STATE_IDLE;
+                    }
+                    else
+                    {
+                        s_eval.state = EVAL_STATE_ERROR;
+                    }
+
+                    break;
+                }
+
+                case EVAL_STATE_PLIM:
+                {
+                    s_eval.status = BMS_Eval_PLim(&data, p_out);
+
+                    if(s_eval.status == EVAL_OK)
+                    {
+                        s_eval.ctx.plim_done = true;
 
                         s_eval.state = EVAL_STATE_IDLE;
                     }
@@ -149,6 +176,7 @@ eval_status_t BMS_Eval_Task(const daq_data_t *p_data_in, eval_con_t *p_out)
                     s_eval.ctx.icon_done = false;
                     s_eval.ctx.vcon_done = false;
                     s_eval.ctx.tcon_done = false;
+                    s_eval.ctx.plim_done = false;
 
                     s_eval.state = EVAL_STATE_IDLE;
 
@@ -157,6 +185,11 @@ eval_status_t BMS_Eval_Task(const daq_data_t *p_data_in, eval_con_t *p_out)
 
                 case EVAL_STATE_ERROR:
                 {
+                    if(s_eval.status == EVAL_OK)
+                    {
+                        s_eval.status = EVAL_UNKNOWN_ERROR;
+                    }
+
                     break;
                 }
             }
@@ -336,6 +369,85 @@ eval_status_t BMS_Eval_Tcon(const daq_data_t *p_data_in, eval_con_t *p_out)
         else
         {
             status = EVAL_TDATA_INVALID;
+        }
+    }
+    else
+    {
+        status = EVAL_NULL_PTR;
+    }
+
+    return status;
+}
+
+eval_status_t BMS_Eval_PLim(const daq_data_t *p_data_in, eval_con_t *p_out)
+{
+    eval_status_t status = EVAL_OK;
+
+    uint32_t limit_mA = 0U;
+    uint32_t plim_mW = 0U;
+
+    if((p_data_in != NULL) && (p_out != NULL))
+    {
+        daq_data_t data = *p_data_in;
+
+        eval_con_t eval = *p_out;
+
+        if(s_eval.is_init)
+        {
+            if((eval.batt_state == BATT_STATE_DCHG) || (eval.batt_state == BATT_STATE_IDLE))
+            {
+                plim_mW = DCL_W;
+            }
+            else if(eval.batt_state == BATT_STATE_CHG)
+            {
+                plim_mW = CCL_W;
+            }
+            else
+            {
+                status = EVAL_UNDEF_STATE;
+            }
+
+            if(status == EVAL_OK)
+            {
+                uint32_t pack_mV = 0U;
+
+                if(data.v_data.v_valid)
+                {
+                    for(uint8_t i = 0U; i < MOD_NUM; i++)
+                    {
+                        for(uint8_t j = 0U; j < MOD_CELL_NUM; j++)
+                        {
+                            pack_mV += data.v_data.vmod[i].cell_val_mV[j];
+                        }
+                    }
+                }
+                else
+                {
+                    status = EVAL_VDATA_INVALID;
+                }
+
+                if((pack_mV <= UINT32_MAX) && (pack_mV != 0U))
+                {
+                    limit_mA = plim_mW / pack_mV;
+                }
+                else
+                {
+                    status = EVAL_VDATA_INVALID;
+                }
+
+                if((eval.batt_state == BATT_STATE_DCHG) || (eval.batt_state == BATT_STATE_IDLE))
+                {
+                    p_out->pwr_lim.dcl_mW = limit_mA;
+                }
+                else if(eval.batt_state == BATT_STATE_CHG)
+                {
+                    p_out->pwr_lim.ccl_mW = limit_mA;
+                }
+            }
+        }
+        else
+        {
+            status = EVAL_NOT_INIT;
         }
     }
     else
